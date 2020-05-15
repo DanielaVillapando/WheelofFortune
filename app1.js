ed4437e54e8379ff05678c8d04e88267566a07f1
feliciawidjaja <55511908+feliciawidjaja@users.noreply.github.com>
2020-05-15 01:36:19 -0700

    Add files via upload

---
+++ app1.js
@@ -0,0 +1,7 @@
+function instructionsBtn() {
+    window.alert('Spin the wheel to see how many points you will gain/lose in the current round. Then, click the alphabets to solve the puzzle on each round.')
+}
+
+function creditsBtn() {
+    window.alert('Joseph Wong\nShahin Yousefi\nDaniela Villapando\nFelicia Widjaja\nWesly Wong')
+}
