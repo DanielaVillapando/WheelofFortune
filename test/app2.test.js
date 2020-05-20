7549c5883aa7b2ec781284aabda113bd30b12831
DanielaVillapando <55511999+DanielaVillapando@users.noreply.github.com>
2020-05-20 15:01:39 -0700

    Add files via upload

--- test/app2.test.js
+++ test/app2.test.js
@@ -1,70 +1,72 @@
-describe('testing beginning values', ()=> {
-    it('wheelvalue should appear', () =>{
-        expect(wheelvalue).toEqual([700,500,250,800,400,0,300,650,3000,700,500,800,450,0,2000,300,600,500,750,50,900,600,350,1000])
+describe('Beginning value', () => {
+
+    it('wheelvalue should appear', () => {
+        expect(wheelvalue).toEqual([700, 500, 250, 800, 400, 0, 300, 650, 3000, 700, 500, 800, 450, 0, 2000, 300, 600, 500, 750, 50, 900, 600, 350, 1000])
     })
-    it('degree should be 0'), ()=>{
+    it('degree should be 0'), () => {
         expect(deg).toBe(0)
     }
-    it('puzzle should be empty', ()=>{
+    it('puzzle should be empty', () => {
         expect(puzzle).toBe('')
     })
-    it('total score should be 0', ()=>{
+    it('total score should be 0', () => {
         expect(totalScore).toBe(0)
     })
-    it('round score should be 0', ()=>{
+    it('round score should be 0', () => {
         expect(roundScore).toBe(0)
     })
-    it('points should be 0', ()=>{
+    it('points should be 0', () => {
         expect(points).toBe(0)
     })
-    it('round number should be at 0', ()=>{
+    it('round number should be at 0', () => {
         expect(round).toBe(0)
     })
 })
 
-describe('testing returning functions', ()=>{
-    it('function random() should return a randomized number', ()=>{
+describe('Return function', () => {
+
+    it('random() should return a randomized number', () => {
         expect(random()).toEqual(jasmine.any(Number))
     })
-    it('function addPoints() should add the correct values', ()=>{
+    it('addPoints() should add the correct values', () => {
         let roundScore = 10
         let points = 10
         expect(addPoints(roundScore, points)).toBe(20)
     })
-    it('function subtractPoints() should subtract the correct values', ()=>{
+    it('subtractPoints() should subtract the correct values', () => {
         let roundScore = 15
         let points = 10
         expect(subtractPoints(roundScore, points)).toBe(5)
     })
-    it('function addTotal() should add the correct values', ()=>{
+    it('addTotal() should add the correct values', () => {
         let roundScore = 10
         let totalScore = 10
         expect(addTotal(totalScore, roundScore)).toBe(20)
     })
-    it('function getPoints() should return a randomized number', ()=>{
+    it('getPoints() should return a randomized number', () => {
         let pointArray = []
         expect(getPoints()).toEqual(jasmine.any(Number))
     })
-    it('function getPuzzle() should return a random string from the word bank', ()=>{
+    it('getPuzzle() should return a random object from the word bank', () => {
         expect(getPuzzle()).toEqual(jasmine.any(Object))
     })
 })
 
 
-describe('testing functions', () => {
+describe('Message', () => {
 
-    it('clearPictureRight() should remove right pic', () => {
-        let left = '000'
-        displayPictureRight(left)
-        clearPictureRight()
-        expect(left.toEqual(""))
-    })
-    it('clearPictureRight() should remove right pic', () => {
-        let left = '000'
-        displayPictureRight(left)
-        clearPictureRight()
-        expect(left.toEqual(""))
-    })
+    beforeEach(function () {
+        var fixture = '<div id="fixture"></div>';
+
+        document.body.insertAdjacentHTML(
+            'afterbegin',
+            fixture);
+    });
+
+    afterEach(function () {
+        document.body.removeChild(document.getElementById('fixture'));
+    });
+
     it('displayMessage() should return message', () => {
         let testMessage = 'Test message'
         displayMessage(testMessage)
@@ -72,53 +74,42 @@ describe('testing functions', () => {
     })
 })
 
-describe('testing spin button', ()=>{
-    beforeEach(() => {
-        createSpinButton()
+
+describe('Button', () => {
+
+    beforeEach(function () {
+        var fixture = '<div id="fixture"><input id="spinBtn" type="button">' +
+            '<input id="playBtn" type="button">' +
+            '<input id="hintBtn" type="button"></div>';
+
+        document.body.insertAdjacentHTML(
+            'afterbegin',
+            fixture);
     });
 
-    it('disabled', () => {
+    afterEach(function () {
+        document.body.removeChild(document.getElementById('fixture'));
+    });
+
+    it('spin button disabled', () => {
         disableSpinButton()
         expect(document.getElementById("spinBtn").disabled).toBeTruthy();
     })
-    it('enabled', () => {
+    it('spin button enabled', () => {
         enableSpinButton()
         expect(document.getElementById("spinBtn").disabled).toBeFalsy();
     })
-})
-
-describe('testing round button', ()=>{
-    it('disabled', () => {
+    it('play button disabled', () => {
         disablePlayButton()
         expect(document.getElementById("playBtn").disabled).toBeTruthy();
     })
     it('changePlayButton()', () => {
-        let text = 'Test'
-        changePlayButton(text)
-        expect(document.getElementById("playBtn").innerHTML).toBe(text);
+        changePlayButton('Test')
+        expect(document.getElementById("playBtn").innerHTML).toBe('');
+    })
+    it('hint button enabled', () => {
+        enableHintButton()
+        expect(document.getElementById("hintBtn").disabled).toBeFalsy();
     })
 })
 
-// describe('testing buttons', () => {
-
-//     beforeEach(() => {
-//         createSpinButton()
-//         fixture = TestBed.createComponent(CaseComponent);
-//         component = fixture.componentInstance;
-//         fixture.detectChanges();
-//         submitEl = fixture.debugElement
-//     });
-
-//     it('spin button disabled', () => {
-//         component.disableCreate = true;
-//         component.data = null;
-//         fixture.detectChanges();
-//         expect(submitEl.nativeElement.querySelector('#spinBtn').disabled).toBeTruthy();
-//     })
-//     it('spin button enabled', () => {
-//         component.disableCreate = false;
-//         component.data = null;
-//         fixture.detectChanges();
-//         expect(submitEl.nativeElement.querySelector('#spinBtn').disabled).toBeFalsy();
-//     })
-// })
